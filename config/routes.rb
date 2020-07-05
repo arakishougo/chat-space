a9fb4692ca1ee119f6fe46537351ec4633d37091
arakishougo <jothscunhvoipk@ezweb.ne.jp>
2020-07-05 17:12:27 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+<<<<<<< Updated upstream
   root "messages#index"
+=======
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+>>>>>>> Stashed changes
 end
